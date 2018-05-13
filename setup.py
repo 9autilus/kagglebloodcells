c8dcdd8d0453602908e0d59719d7fe3dcf21bc60
Govind <govindnh4cl@gmail.com>
2018-05-12 20:54:19 -0400

    Initial commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='KaggleBloodCells',
+    version='1.0',
+    description='Add custom files in kaggle kernel',
+    author='Govind',
+    author_email='example@example.com',
+    packages=['kaggle_blood_cells'],  #same as name
+)
